0188024fe7325fb5ca7713a50c056a26d72bcb90
P. Vaibhav Goud <pv22btb0a43@student.nitw.ac.in>
2026-02-08 16:34:49 +0530

    React setup with tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-blue-600">Naksh Jewels Store</h1>
+    </div>
+  )
+}
+
+export default App
